9fbf1e49f9beeecd64bb8218d3b5d25eca0569c0
NurikD <duishoev0123@gmail.com>
2025-09-09 17:33:44 +0500

    не уверен что рабочая версия

--- backend/settings.py
+++ backend/settings.py
@@ -133,8 +133,12 @@ from datetime import timedelta
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticated",
+    ),
 }
+
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from django.contrib import admin
-from documents.views import DocumentListAPI, DocumentDownloadAPI, DocumentReplaceAPI, DocumentDeleteAPI
+from documents.views import DocumentListAPI, DocumentDownloadAPI, DocumentReplaceAPI, DocumentDeleteAPI, DocumentDetailAPI
 from accounts.views import LoginView  # твой TokenObtainPairView кастомный
 
 urlpatterns = [
@@ -15,5 +15,7 @@ urlpatterns = [
     path("api/documents/", DocumentListAPI.as_view()),
     path("api/documents/<int:pk>/download/", DocumentDownloadAPI.as_view()),
     path("api/documents/<int:pk>/replace/", DocumentReplaceAPI.as_view()),  # admin only
-    path("api/documents/<int:pk>/delete/", DocumentDeleteAPI.as_view()),    # admin only
+    path("api/documents/<int:pk>/delete/", DocumentDeleteAPI.as_view()),
+    path("api/documents/<int:pk>/", DocumentDetailAPI.as_view()),
+    # admin only
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # только для dev
